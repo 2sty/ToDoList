0ae22902f5c1a116b44aef4ada2922e90bd6bba5
2sty <atlustowski@gmail.com>
2020-08-31 22:00:35 +0200

    add simple funcionality and css

--- app.js
+++ app.js
@@ -3,21 +3,30 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Buy cake", "Cook cake", "Eat cake"];
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
 app.set('view engine', 'ejs');
 
 app.get("/", function(req,res){
   //res.send("Hello!");
-  var today = new Date();
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
-  var day = today.toLocaleDateString("en-US", options);
-  res.render('list', {kindOfDay: day});
+  let day = today.toLocaleDateString("en-US", options);
+  res.render('list', {kindOfDay: day, newListItems: items});
 
 });
-
+app.post("/", function(req,res){
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
 app.listen(3000, function(){
   console.log("Starting on port 3000!");
 })
