a76580107888a7c02585b3350c10aa414b341c60
2sty <atlustowski@gmail.com>
2020-09-12 13:43:04 +0200

    add own date module and export to app.js, refatore code

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-var items = ["Buy cake", "Cook cake", "Eat cake"];
+const items = ["Buy cake", "Cook cake", "Eat cake"];
+const workItems = [];
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
@@ -12,21 +14,29 @@ app.set('view engine', 'ejs');
 
 app.get("/", function(req,res){
   //res.send("Hello!");
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-  let day = today.toLocaleDateString("en-US", options);
-  res.render('list', {kindOfDay: day, newListItems: items});
+  const day = date.getDate();
+  res.render('list', {listTitle: day, newListItems: items});
 
 });
 app.post("/", function(req,res){
-  let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  console.log(req.body);
+  const item = req.body.newItem;
+  if(req.body.list === "Work list"){
+    workItems.push(item);
+    res.redirect("/work");
+  }else{
+    items.push(item);
+    res.redirect("/");
+  }
 });
+app.get("/work", function(req,res){
+  res.render('list', {listTitle: "Work list", newListItems: workItems});
+});
+app.post("/work", function(req,res){
+  const item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
 app.listen(3000, function(){
   console.log("Starting on port 3000!");
 })
